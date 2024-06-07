28d4b1a4eaf05ab3bd8ad143b4a14df491cddb83
Syosen33 <syosen33@gmail.com>
2024-06-07 14:21:35 +0900

    サイドバーとチャット画面のレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # ↑↑上の記述は削除しましょう↑↑
   root to: "messages#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
