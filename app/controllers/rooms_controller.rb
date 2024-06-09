fd6592e0b57c1051a6ddec6e5ae761730f598220
Syosen33 <syosen33@gmail.com>
2024-06-10 05:49:17 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,12 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
-end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
